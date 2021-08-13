122cc0d3f6023ede51428c090c9399a9542957ad
lukiwa <lukasz.iwan.99@gmail.com>
2021-08-14 01:53:11 +0200

    Moved implementation to .cpp file

--- src/OpenGL/VertexAttributes.cpp
+++ src/OpenGL/VertexAttributes.cpp
@@ -1,1 +1,40 @@
 #include "VertexAttributes.h"
+void VertexAttributes::Bind()
+{
+
+    uint32_t offset = 0;
+    for (std::size_t i = 0; i < _attributes.size(); ++i) {
+        const auto& attribute = _attributes[i];
+        GlAssert(glEnableVertexAttribArray(i));
+        GlAssert(glVertexAttribPointer(
+            i, attribute.count, attribute.type, attribute.normalized, _stride, (const void*)offset));
+        offset += attribute.count * GlTypeToSize(attribute.type);
+    }
+}
+
+/**
+ * Do not normalize!!!
+ * @param count
+ * @param type
+ */
+void VertexAttributes::AddAttribute(uint32_t count, GLenum type)
+{
+    VertexAttributeElement attribute = { count, type, GL_FALSE };
+    _stride += count * GlTypeToSize(type);
+    _attributes.push_back(attribute);
+}
+
+uint32_t VertexAttributes::GlTypeToSize(GLenum type)
+{
+    switch (type) {
+    case GL_FLOAT:
+        return sizeof(GLfloat);
+    case GL_INT:
+        return sizeof(GLint);
+    case GL_UNSIGNED_INT:
+        return sizeof(GLuint);
+    default:
+        LOG_WARNING("RETREVING SIZE OF UNEXPECTED TYPE!");
+        return 0;
+    }
+}
--- src/OpenGL/VertexAttributes.h
+++ src/OpenGL/VertexAttributes.h
@@ -8,44 +8,12 @@
 
 class VertexAttributes {
 public:
-    void Bind()
-    {
-        uint32_t offset = 0;
-        for (std::size_t i = 0; i < _attributes.size(); ++i) {
-            const auto& attribute = _attributes[i];
-            GlAssert(glEnableVertexAttribArray(i));
-            GlAssert(glVertexAttribPointer(
-                i, attribute.count, attribute.type, attribute.normalized, _stride, (const void*)offset));
-            offset += attribute.count * GlTypeToSize(attribute.type);
-        }
-    }
-    /**
-     * Do not normalize!!!
-     * @param count
-     * @param type
-     */
-    void AddAttribute(uint32_t count, GLenum type)
-    {
-        VertexAttributeElement attribute = { count, type, GL_FALSE };
-        _stride += count * GlTypeToSize(type);
-        _attributes.push_back(attribute);
-    }
+    void Bind();
+
+    void AddAttribute(uint32_t count, GLenum type);
 
 private:
-    inline static uint32_t GlTypeToSize(GLenum type)
-    {
-        switch (type) {
-        case GL_FLOAT:
-            return sizeof(GLfloat);
-        case GL_INT:
-            return sizeof(GLint);
-        case GL_UNSIGNED_INT:
-            return sizeof(GLuint);
-        default:
-            LOG_WARNING("RETREVING SIZE OF UNEXPECTED TYPE!");
-            return 0;
-        }
-    }
+    static uint32_t GlTypeToSize(GLenum type);
 
 private:
     struct VertexAttributeElement {
