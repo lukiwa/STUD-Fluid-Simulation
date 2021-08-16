32448c76c73c49e57f5f88fd92a94727da2a7b70
lukiwa <lukasz.iwan.99@gmail.com>
2021-08-17 00:02:51 +0200

    Add drawying abstraction

---
+++ src/OpenGL/Renderer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Tracing.h"
+#include "IndexBuffer.h"
+#include "PixelMap.h"
+#include "ShaderProgram.h"
+#include "VertexArray.h"
+#include <vector>
+class Renderer {
+public:
+    Renderer(VertexArray& vao, IndexBuffer& ibo, ShaderProgram& program, PixelMap& pixelMap);
+    ~Renderer() = default;
+
+    void Draw() const;
+
+    void ClearColor(const std::vector<int>& components) const;
+
+    void Clear() const;
+
+private:
+    VertexArray& _vao;
+    IndexBuffer& _ibo;
+    ShaderProgram& _program;
+    PixelMap& _pixelMap;
+};
