efbfc87ce90e84aea352b415b8a698bd8f982d7f
lukiwa <lukasz.iwan.99@gmail.com>
2021-08-17 00:05:06 +0200

    Add random helper class

---
+++ src/Random.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <random>
+
+class Random {
+    inline static std::mt19937 _random_engine{};
+
+public:
+    static void Seed()
+    {
+        _random_engine.seed(std::random_device()());
+    }
+
+    static double Double(double min, double max)
+    {
+        std::uniform_real_distribution<double> distribution(min, max);
+        return (distribution(_random_engine));
+    }
+
+    static double Int(int min, int max)
+    {
+        std::uniform_int_distribution<int> distribution(min, max);
+        return (distribution(_random_engine));
+    }
+};
