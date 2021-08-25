5d1671eb91354ace01ecf42d1b5e0acdea1e2b5d
lukiwa <lukasz.iwan.99@gmail.com>
2021-08-25 23:10:29 +0200

    Rework pixelmap to use interfaces + factory. Do not map and unmap buffers so often

--- src/OpenGL/PixelMap.h
+++ src/OpenGL/PixelMap.h
@@ -1,28 +1,52 @@
 #pragma once
+#include "Dimensions.h"
+#include "IBoundable.h"
 #include "PixelBufferObject.h"
 #include "Texture.h"
 #include <cassert>
 #include <vector>
 
-class PixelMap {
+class IPixelMapComponentsFactory {
 public:
-    void Bind() const;
-    void Unbind() const;
+    using Components = std::pair<std::unique_ptr<IPixelBufferObject>, std::unique_ptr<ITexture>>;
+    virtual ~IPixelMapComponentsFactory() = default;
+    virtual Components CreateComponents(
+        Dimensions dimensions, int internalPixelFormat, GLenum dataType, const void* data) const = 0;
+};
 
-    PixelMap(int width, int height, int type);
+class PixelMapComponentsFactory : public IPixelMapComponentsFactory {
+public:
+    virtual Components CreateComponents(
+        Dimensions dimensions, int internalPixelFormat, GLenum dataType, const void* data) const override
+    {
+        std::unique_ptr<IPixelBufferObject> pbo(new PixelBufferObject(dimensions.x, dimensions.y, internalPixelFormat));
+        std::unique_ptr<ITexture> texture(new Texture(
+            dimensions.z == 0 ? GL_TEXTURE_2D : GL_TEXTURE_3D, dimensions, internalPixelFormat, dataType, data));
+        texture->SetTextureParameters();
+        return { std::move(pbo), std::move(texture) };
+    }
+};
+
+class PixelMap : public IBoundable {
+public:
+    void Bind() const override;
+    void Unbind() const override;
+
+    PixelMap(Dimensions dimensions, int internalPixelFormat, IPixelMapComponentsFactory* factory);
     ~PixelMap() = default;
 
     void SetAllPixels(const std::vector<int>& components);
-
     void SetPixel(int x, int y, const std::vector<int>& components);
+    void SwapBuffer();
 
-    void Clear() const;
+    void Clear();
+
 private:
     int _pixelType;
     int _width;
     int _height;
-    int _type;
 
-    PixelBufferObject _pbo;
-    std::unique_ptr<Texture> _texture;
+    std::unique_ptr<IPixelBufferObject> _pbo;
+    std::unique_ptr<ITexture> _texture;
+    std::vector<GLubyte> _pixelBuffer;
 };
