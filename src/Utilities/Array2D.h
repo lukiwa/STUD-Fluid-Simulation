d8f74ec9961df5fe1c2bbfce6c6ee64d1f64b349
lukiwa <lukasz.iwan.99@gmail.com>
2021-10-10 17:32:05 +0200

    Add Array2D helper class

--- src/Utilities/Array2D.h
+++ src/Utilities/Array2D.h
@@ -3,26 +3,31 @@
 #include <algorithm>
 #include <iostream>
 
-template<typename T>
-class Array2D {
+template <typename T> class Array2D {
     int _size;
-    T *array;
+    T* array;
+
 public:
-    explicit Array2D(int size) :
-        _size(size) {
+    explicit Array2D(int size)
+        : _size(size)
+    {
         array = new T[size * size];
+        std::fill(array, array + (size * size), 0);
     }
 
-    ~Array2D() {
+    ~Array2D()
+    {
         delete[] array;
     }
 
-    Array2D(const Array2D &other) {
+    Array2D(const Array2D& other)
+    {
         _size = other._size;
         std::copy(other.array, other.array + other._size, array);
     }
 
-    Array2D &operator=(const Array2D &other) {
+    Array2D& operator=(const Array2D& other)
+    {
         if (&other != this) {
             _size = other._size;
             std::copy(other.array, other.array + other._size, array);
@@ -30,33 +35,48 @@ public:
         return *this;
     }
 
-    int GetSize() const{
+    int GetSize() const
+    {
         return _size;
     }
 
+    T& operator()(int x, int y) const
+    {
+#ifdef DEBUG
+        return at(x, y);
+#else
+        return array[x + y * _size];
+#endif
+    }
 
-    T &operator()(int x, int y) const {
+    T& operator()(int x, int y)
+    {
+#ifdef DEBUG
+        return at(x, y);
+#else
+        return array[x + y * _size];
+#endif
+    }
+
+    T& at(int x, int y) const
+    {
+        if (x >= _size) {
+            throw std::out_of_range("Array out of bound");
+        }
+        if (y >= _size) {
+            throw std::out_of_range("Array out of bound");
+        }
         return array[x + y * _size];
     }
 
-    T &operator()(int x, int y) {
+    T& at(int x, int y)
+    {
+        if (x >= _size) {
+            throw std::out_of_range("Array out of bound");
+        }
+        if (y >= _size) {
+            throw std::out_of_range("Array out of bound");
+        }
         return array[x + y * _size];
     }
-
-    T &at(int x, int y) const {
-        if (x >= _size) { throw std::out_of_range("Array out of bound"); }
-        if (y >= _size) { throw std::out_of_range("Array out of bound"); }
-        return array[x + y * _size];
-    }
-
-    T &at(int x, int y) {
-        if (x >= _size) { throw std::out_of_range("Array out of bound"); }
-        if (y >= _size) { throw std::out_of_range("Array out of bound"); }
-        return array[x + y * _size];
-    }
-
 };
-
-
-
-
