68cffc50221df77046419ebd2a502e2a76c86102
lukiwa <lukasz.iwan.99@gmail.com>
2021-10-10 17:33:52 +0200

    Simulation algorithms

--- src/Simulation/FluidSimulation.cpp
+++ src/Simulation/FluidSimulation.cpp
@@ -1,4 +1,5 @@
 #include "FluidSimulation.h"
+#include "../Utilities/Tracing.h"
 
 /**
  * Constructs new fluid simulation abstraction
@@ -14,6 +15,7 @@ FluidSimulation::FluidSimulation(Dimensions dimensions, double diffusion, double
     , _diffusion(diffusion)
     , _viscosity(viscosity)
     , _dt(dt)
+    , _iterations(4)
     , _velocityX(_size)
     , _velocityY(_size)
     , _density(_size)
@@ -31,9 +33,7 @@ FluidSimulation::FluidSimulation(Dimensions dimensions, double diffusion, double
  */
 const Array2D<double>& FluidSimulation::CurrentDensityMap() const
 {
-    {
-        return _density.current;
-    }
+    return _density.current;
 }
 
 /**
@@ -51,7 +51,7 @@ void FluidSimulation::AddVelocity(int x, int y, double amountX, double amountY)
 }
 
 /**
- * Add density on given field
+ * Add density on given field. Normalization based on time step
  * (imagine dropping a drop of soy sauce into the water)
  * @param x x coordinate
  * @param y y coordinate
@@ -59,7 +59,8 @@ void FluidSimulation::AddVelocity(int x, int y, double amountX, double amountY)
  */
 void FluidSimulation::AddDensity(int x, int y, double amount)
 {
-    _density.current(x, y) += amount;
+    //normalize
+    _density.current(x, y) = (_density.current(x, y) + _dt * amount) / (1.0f + _dt);
 }
 
 /**
@@ -67,5 +68,253 @@ void FluidSimulation::AddDensity(int x, int y, double amount)
  */
 void FluidSimulation::Step()
 {
-    // TODO
+    VelocityStep();
+    DensityStep();
+}
+/**
+ * Evolve velocities due to:
+ * - addition of forces (done even before Step method in AddVelocity)
+ * - diffusion
+ * - advection
+ */
+void FluidSimulation::VelocityStep()
+{
+    Diffuse(BoundaryType::VERTICAL, _velocityX.previous, _velocityX.current, _viscosity);
+    Diffuse(BoundaryType::HORIZONTAL, _velocityY.previous, _velocityY.current, _viscosity);
+
+    Project(_velocityX.previous, _velocityY.previous, _velocityX.current, _velocityY.current);
+
+    Advect(BoundaryType::VERTICAL, _velocityX.current, _velocityX.previous, _velocityX.previous, _velocityY.previous);
+
+    Advect(BoundaryType::HORIZONTAL, _velocityY.current, _velocityY.previous, _velocityX.previous, _velocityY.previous);
+
+    Project(_velocityX.current, _velocityY.current, _velocityX.previous, _velocityY.previous);
+}
+
+/**
+ * Evolve density due to: addition of forces, addition of density, moving density along velocities, and diffusion
+ */
+void FluidSimulation::DensityStep()
+{
+    Diffuse(BoundaryType::ALL, _density.previous, _density.current, _diffusion);
+    Advect(BoundaryType::ALL, _density.current, _density.previous, _velocityX.current, _velocityY.current);
+}
+
+/**
+ * Diffuse, spread medium across the grid no matter what velocity is.
+ * To make this method stable, instead of calculating next surrounding values (and thus using linear interpolation),
+ * a method of finding current values from the next ones is used. However this needs a solver - see a LinearSolve
+ * method.
+ * @param bound where a boundary condition should be st
+ * @param medium medium that should be diffused
+ * @param prevMedium previous medium
+ * @param spreadSpeed viscosity/diffusion
+ */
+void FluidSimulation::Diffuse(
+    FluidSimulation::BoundaryType bound, Array2D<double>& medium, Array2D<double>& prevMedium, double spreadSpeed)
+{
+    double k = _dt * spreadSpeed * (_size - 2) * (_size - 2);
+
+    LinearSolve(bound, medium, prevMedium, k, 1 + 4 * k);
+}
+
+/**
+ * Use Gauss-Seidel method to solve linear equations
+ * @param bound depending on what parameter is currently being solved
+ * @param medium used medium
+ * @param prevMedium previous medium
+ * @param k amount of how much should the medium change
+ * @param c denominator
+ */
+void FluidSimulation::LinearSolve(
+    FluidSimulation::BoundaryType bound, Array2D<double>& medium, Array2D<double>& prevMedium, double k, double c)
+{
+    for (int i = 0; i < _iterations; ++i) {
+        for (int x = 1; x < _size - 1; ++x) {
+            for (int y = 1; y < _size - 1; ++y) {
+                medium(x, y) = (prevMedium(x, y)
+                                   + k * (medium(x + 1, y) + medium(x - 1, y) + medium(x, y + 1) + medium(x, y - 1)))
+                    / c;
+            }
+        }
+        SetBoundaryConditions(bound, medium);
+    }
+}
+
+/**
+ * After all operations on fluid it is no longer divergence-free. This step uses Helmholtz decomposition
+ * in order to get divergence-free fluid. This the fluid after this step will have a lot of "curl"
+ * @param velocityX matrix of X velocities at each point
+ * @param velocityY matrix of Y velocities at each point
+   NOTE: Those two parameters are passed only to avoid allocating additional memory - they will be recalculated soon
+         anyways
+ * @param p curl free part
+ * @param divergence divergence part
+ */
+void FluidSimulation::Project(
+    Array2D<double>& velocityX, Array2D<double>& velocityY, Array2D<double>& p, Array2D<double>& divergence)
+{
+    for (int x = 1; x < _size - 1; ++x) {
+        for (int y = 1; y < _size - 1; ++y) {
+
+            // calculate "divergence" velocities
+            divergence(x, y) = -0.5f
+                * (velocityX(x + 1, y) - velocityX(x - 1, y) + velocityY(x, y + 1) - velocityY(x, y - 1)) / _size;
+            p(x, y) = 0;
+        }
+    }
+
+    SetBoundaryConditions(BoundaryType::ALL, divergence);
+    SetBoundaryConditions(BoundaryType::ALL, p);
+
+    // make use of the solver to calculate p - a scalar value
+    // gradient of p will become a curl free part, since curl of a gradient field is 0
+    LinearSolve(BoundaryType::VERTICAL, p, divergence, 1, 4);
+
+    for (int x = 1; x < _size - 1; x++) {
+        for (int y = 1; y < _size - 1; y++) {
+
+            velocityX(x, y) -= 0.5f * (p(x + 1, y) - p(x - 1, y)) * _size;
+
+            velocityY(x, y) -= 0.5f * (p(x, y + 1) - p(x, y - 1)) * _size;
+        }
+    }
+
+    SetBoundaryConditions(BoundaryType::VERTICAL, velocityX);
+    SetBoundaryConditions(BoundaryType::HORIZONTAL, velocityY);
+}
+
+/**
+ * Calculate linear interpolation between a and b for the parameter k (aka. std::lerp)
+ * @param a first value
+ * @param b second value
+ * @param k interpolation point between a and b
+ * @return linear interpolation between a and b
+ */
+double FluidSimulation::LinearInterpolation(double a, double b, double k) const
+{
+    return a + k * (b - a);
+}
+
+/**
+ * Calculate how density will move due to velocities. A "back tracing" method is used -
+ * taking previous density value at x,y then trace from where this value comes from. The result will be
+ * somewhere in between grids, so a linear interpolation is used.
+ * @param bound boundary
+ * @param medium current density
+ * @param prevMedium previous density
+ * @param velocityX current X velocity matrix
+ * @param velocityY current Y velocity matrix
+ */
+void FluidSimulation::Advect(FluidSimulation::BoundaryType bound, Array2D<double>& medium,
+    const Array2D<double>& prevMedium, const Array2D<double>& velocityX, const Array2D<double>& velocityY)
+{
+    // velocities for x and y coordinates
+    double dtX = _dt * (_size - 2);
+    double dtY = _dt * (_size - 2);
+
+    // coordinates of point that was back traced
+    double fx, fy;
+
+    // integer parts of the back traced point
+    int iX, iY;
+
+    // fractional parts of the back traced point
+    double jX, jY;
+
+    for (int x = 1; x < _size - 1; ++x) {
+        for (int y = 1; y < _size - 1; ++y) {
+
+            // trace back where next density for i,j comes from
+            fx = x - dtX * velocityX(x, y);
+            fy = y - dtY * velocityY(x, y);
+
+            // do not access points outside of the box during interpolation
+            if (fx < 0.5) {
+                fx = 0.5;
+            }
+            if (fx > _size + 0.5) {
+                fx = _size - 2;
+            }
+            if (fy < 0.5) {
+                fy = 0.5;
+            }
+            if (fy > _size + 0.5) {
+                fy = _size - 2;
+            }
+
+            // integer part of coordinate
+            iX = std::min(_size - 2, static_cast<int>(std::floor(fx)));
+            iY = std::min(_size - 2, static_cast<int>(std::floor(fy)));
+
+            // fractional part of coordinate
+            jX = fx - iX;
+            jY = fy - iY;
+
+            // interpolate lower values (left and right bottom corner)
+            auto z1 = LinearInterpolation(prevMedium(iX, iY), prevMedium(iX + 1, iY), jX);
+
+            // interpolate upper values (left and right top corner)
+            auto z2 = LinearInterpolation(prevMedium(iX, iY + 1), prevMedium(iX + 1, iY + 1), jX);
+
+            medium(x, y) = LinearInterpolation(z1, z2, jY);
+        }
+    }
+
+    SetBoundaryConditions(bound, medium);
+}
+
+/**
+ * The fluid is assumed to be contained in a box so this function assures that:
+ * - if a medium is a velocity: A No-Slip condition is met, and the appropriate velocity component is 0 relative
+ *   to a boundary. That essentially means setting last cell near boundary minus penultimate cell
+ *   (Prandtl no-slip boundary condition)
+ *   (https://en.wikipedia.org/wiki/Boundary_layer https://en.wikipedia.org/wiki/No-slip_condition)
+ * - if a medium is a density: As stated in: https://www.dgp.toronto.edu/public_user/stam/reality/Research/pdf/GDC03.pdf
+ *   we assume continuity - in case of density, last cells should be the same as penultimate cell
+ * @param bound which type of boundary contidion will be handled
+ * @param medium medium on which boundary contidion will be set
+ */
+void FluidSimulation::SetBoundaryConditions(FluidSimulation::BoundaryType bound, Array2D<double>& medium)
+{
+
+    for (int i = 0; i < _size - 1; ++i) {
+        switch (bound) {
+        case BoundaryType::VERTICAL:
+            // last cells near vertical wall will be equal minus penultimate cell value
+            // so the relative velocity to the boundary will be 0
+            medium(0, i) = -medium(1, i);
+            medium(_size - 1, i) = -medium(_size - 2, i);
+
+            // last cells near horizontal wall will be equal to penultimate cell value
+            medium(i, 0) = medium(i, 1);
+            medium(i, _size - 1) = medium(i, _size - 2);
+            break;
+        case BoundaryType::HORIZONTAL:
+            // same approach as above, but for movement along Y coordinate
+            medium(i, 0) = -medium(i, 1);
+            medium(i, _size - 1) = -medium(i, _size - 2);
+
+            medium(0, i) = medium(1, i);
+            medium(_size - 1, i) = medium(_size - 2, i);
+            break;
+        case BoundaryType::ALL:
+            // in case of density, last cells should be the same as penultimate cell
+            medium(i, 0) = medium(i, 1);
+            medium(i, _size - 1) = medium(i, _size - 2);
+
+            medium(0, i) = medium(1, i);
+            medium(_size - 1, i) = medium(_size - 2, i);
+            break;
+
+        default:
+            LOG_ERROR("Boundary type not handled in switch!\n");
+        }
+    }
+
+    // boundaries at the corners, each corner is the average of it's neighbours
+    medium(0, 0) = 0.5f * (medium(1, 0) + medium(0, 1));
+    medium(0, _size - 1) = 0.5f * (medium(1, _size - 1) + medium(0, _size - 2));
+    medium(_size - 1, 0) = 0.5f * (medium(_size - 2, 0) + medium(_size - 1, 1));
+    medium(_size - 1, _size - 1) = 0.5f * (medium(_size - 2, _size - 1) + medium(_size - 1, _size - 2));
 }
--- src/Simulation/FluidSimulation.h
+++ src/Simulation/FluidSimulation.h
@@ -1,7 +1,9 @@
 #pragma once
-#include "IFluid.h"
 #include "../Utilities/Array2D.h"
 #include "../Utilities/Dimensions.h"
+#include "IFluid.h"
+#include <chrono>
+#include <cmath>
 
 class FluidSimulation : public IFluidSimulation {
 public:
@@ -19,13 +21,27 @@ public:
         Array2D<double> current;
     };
 
-    const Array2D<double>& CurrentDensityMap() const override;
+    enum class BoundaryType { VERTICAL, HORIZONTAL, ALL };
 
+    const Array2D<double>& CurrentDensityMap() const override;
     void AddVelocity(int x, int y, double amountX, double amountY) override;
     void AddDensity(int x, int y, double amount) override;
     void Step() override;
 
 private:
+    void Diffuse(BoundaryType bound, Array2D<double>& medium, Array2D<double>& prevMedium, double spreadSpeed);
+    void LinearSolve(BoundaryType bound, Array2D<double>& medium, Array2D<double>& prevMedium, double k, double c);
+    void Project(
+        Array2D<double>& velocityX, Array2D<double>& velocityY, Array2D<double>& p, Array2D<double>& divergence);
+    void Advect(BoundaryType bound, Array2D<double>& medium, const Array2D<double>& prevMedium,
+        const Array2D<double>& velocityX, const Array2D<double>& velocityY);
+    void SetBoundaryConditions(BoundaryType bound, Array2D<double>& medium);
+
+private:
+    void VelocityStep();
+    void DensityStep();
+    double LinearInterpolation(double a, double b, double k) const;
+
     friend class FluidBuilder;
     FluidSimulation(Dimensions dimensions, double diffusion, double viscosity, double dt);
 
@@ -33,8 +49,8 @@ private:
     double _diffusion;
     double _viscosity;
     double _dt;
+    int _iterations;
     State _velocityX;
     State _velocityY;
     State _density;
 };
-
