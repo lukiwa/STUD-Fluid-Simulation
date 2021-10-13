f5556401fa1c8a4193f4a195fa2ac5d6de75acd6
lukiwa <lukasz.iwan.99@gmail.com>
2021-10-14 01:11:35 +0200

    Refactored tests

--- test/OpenGL/PixelMap.cpp
+++ test/OpenGL/PixelMap.cpp
@@ -1,12 +1,10 @@
+#include "PixelMap.h"
+#include "PixelBufferObject.h"
+#include "Texture.h"
 #include "Tracing.h"
 #include "Window.h"
 #include "gtest/gtest.h"
-#include <GL/glew.h>
-#include <gmock/gmock.h>
-
-#include "PixelBufferObject.h"
-#include "PixelMap.h"
-#include "Texture.h"
+#include <memory>
 
 class MockPixelBufferObject : public IPixelBufferObject {
 public:
@@ -67,66 +65,66 @@ public:
     }
 };
 
-TEST(PixelMapTests, RealImplementationReturnsNonNullptrMappedBuffer)
-{
-    ALLOW_DISPLAY;
-    const int width = 255;
-    const int height = 255;
-    const GLenum pixelFormat = GL_RGBA;
+class PixelMapTestFixture : public ::testing::Test {
+public:
+    PixelMapTestFixture()
+        : _width(255)
+        , _height(255)
+        , _pixelFormat(GL_RGBA)
+        , _window(nullptr)
+        , _pixelMap(nullptr)
+    {
+    }
 
+    void SetUp() override
+    {
+        ALLOW_DISPLAY;
+        ASSERT_NO_THROW(_window.reset(new GLFW::Window(10, 10, "Test")));
+        ASSERT_EQ(glewInit(), GLEW_OK);
+
+        _componentsFactory.reset(new PixelMapComponentsFactory());
+        _pixelMap.reset(new PixelMap({ _width, _height, 0 }, _pixelFormat, _componentsFactory.get()));
+        _pixelMap->Clear();
+    }
+
+    void TearDown() override
+    {
+        _pixelMap.reset(nullptr);
+        _componentsFactory.reset(nullptr);
+        _window.reset(nullptr);
+    }
+
+    int _width;
+    int _height;
+    int _pixelFormat;
     std::unique_ptr<GLFW::Window> _window;
-    ASSERT_NO_THROW(_window.reset(new GLFW::Window(10, 10, "Test")));
-    ASSERT_EQ(glewInit(), GLEW_OK);
+    std::unique_ptr<PixelMap> _pixelMap;
+    std::unique_ptr<PixelMapComponentsFactory> _componentsFactory;
+};
 
-    PixelBufferObject pbo(width, height, pixelFormat);
+TEST_F(PixelMapTestFixture, RealImplementationReturnsNonNullptrMappedBuffer)
+{
+    PixelBufferObject pbo(_width, _height, _pixelFormat);
     pbo.Bind();
     ASSERT_NE(pbo.MapBuffer(), nullptr);
 }
 
-TEST(PixelMapTests, MockPixelInternalFormatMatchesRGBA)
+TEST_F(PixelMapTestFixture, PixelInternalFormatMatchesRGBA)
 {
-    const int width = 255;
-    const int height = 255;
-    const GLenum pixelFormat = GL_RGBA;
-    const int pixelFormatNumber = PixelBufferObject::ConvertFormatToNumber(pixelFormat);
+    const int pixelFormatNumber = PixelBufferObject::ConvertFormatToNumber(_pixelFormat);
 
-    std::unique_ptr<IPixelMapComponentsFactory> factory(new MockComponentFactory);
-    PixelMap pixelMap({ width, height, 0 }, pixelFormat, factory.get());
-
-    pixelMap.SetPixel(1, 1, { 255, 255, 255, 255 });
-    auto pixel = pixelMap.GetPixel(1, 1);
+    _pixelMap->SetPixel(1, 1, { 255, 255, 255, 255 });
+    auto pixel = _pixelMap->GetPixel(1, 1);
 
     ASSERT_EQ(pixel.size(), pixelFormatNumber);
 }
 
-TEST(PixelMapTests, MockPixelInternalFormatMatchesRGB)
+TEST_F(PixelMapTestFixture, PixelComponentsMatches)
 {
-    const int width = 255;
-    const int height = 255;
-    const GLenum pixelFormat = GL_RGB;
-    const int pixelFormatNumber = PixelBufferObject::ConvertFormatToNumber(pixelFormat);
+    const int pixelFormatNumber = PixelBufferObject::ConvertFormatToNumber(_pixelFormat);
 
-    std::unique_ptr<IPixelMapComponentsFactory> factory(new MockComponentFactory);
-    PixelMap pixelMap({ width, height, 0 }, pixelFormat, factory.get());
-
-    pixelMap.SetPixel(1, 1, { 255, 255, 255 });
-    auto pixel = pixelMap.GetPixel(1, 1);
-
-    ASSERT_EQ(pixel.size(), pixelFormatNumber);
-}
-
-TEST(PixelMapTests, MockPixelComponentsMatches)
-{
-    const int width = 255;
-    const int height = 255;
-    const GLenum pixelFormat = GL_RGBA;
-    const int pixelFormatNumber = PixelBufferObject::ConvertFormatToNumber(pixelFormat);
-
-    std::unique_ptr<IPixelMapComponentsFactory> factory(new MockComponentFactory);
-    PixelMap pixelMap({ width, height, 0 }, pixelFormat, factory.get());
-
-    pixelMap.SetPixel(1, 1, { 100, 100, 100, 100 });
-    auto pixel = pixelMap.GetPixel(1, 1);
+    _pixelMap->SetPixel(1, 1, { 100, 100, 100, 100 });
+    auto pixel = _pixelMap->GetPixel(1, 1);
 
     ASSERT_EQ(pixel.size(), pixelFormatNumber);
     for (const auto& component : pixel) {
@@ -134,34 +132,25 @@ TEST(PixelMapTests, MockPixelComponentsMatches)
     }
 }
 
-TEST(PixelMapTests, MockPixelsChangeOnlyOnGivenRange)
+TEST_F(PixelMapTestFixture, PixelsChangeOnlyOnGivenRange)
 {
-    const int width = 100;
-    const int height = 100;
-
-    const int widthRangeToChange = width / 2;
-    const int heightRangeToChange = height / 2;
-
-    const GLenum pixelFormat = GL_RGBA;
-
-    std::unique_ptr<IPixelMapComponentsFactory> factory(new MockComponentFactory);
-    PixelMap pixelMap({ width, height, 0 }, pixelFormat, factory.get());
-    pixelMap.Clear();
+    const int widthRangeToChange = _width / 2;
+    const int heightRangeToChange = _height / 2;
 
     for (int x = 0; x < widthRangeToChange; ++x) {
         for (int y = 0; y < heightRangeToChange; ++y) {
-            pixelMap.SetPixel(x, y, { 100, 100, 100, 100 });
+            _pixelMap->SetPixel(x, y, { 100, 100, 100, 100 });
 
-            auto pixel = pixelMap.GetPixel(x, y);
+            auto pixel = _pixelMap->GetPixel(x, y);
             for (const auto& component : pixel) {
                 ASSERT_EQ(component, 100);
             }
         }
     }
 
-    for (int x = widthRangeToChange; x < width; ++x) {
-        for (int y = heightRangeToChange; y < height; ++y) {
-            auto pixel = pixelMap.GetPixel(x, y);
+    for (int x = widthRangeToChange; x < _width; ++x) {
+        for (int y = heightRangeToChange; y < _height; ++y) {
+            auto pixel = _pixelMap->GetPixel(x, y);
             for (const auto& component : pixel) {
                 ASSERT_EQ(component, 255);
             }
